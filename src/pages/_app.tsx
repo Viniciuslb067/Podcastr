7a891f4d0b8bf89a786d00ea0e7aac6cbc09357f
Viniciuslb <viniciuslb067@gmail.com>
2021-04-22 14:41:23 -0300

    fix button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,52 @@
 import "../styles/global.scss";
 
-import { Header } from "../components/Header"
-import { Player } from "../components/Player"
+import { useState } from "react";
+
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+import { PlayerContext } from "../contexts/PlayerContext";
 
 import styles from "../styles/app.module.scss";
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPLaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPLaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
-  )
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPLaying,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
